d1bcb64dea5820b7269562a64bbc7cebb8bdc0b9
xavier <xavier@localdomain.com>
2020-03-02 23:11:42 +0800

    fix local GEO DB

--- js/geoip.js
+++ js/geoip.js
@@ -1,3 +1,58 @@
+function setLocalStorage(blob) {
+    if (window.FileReader) {
+        var a = new FileReader();
+        a.onload = function (e) { localStorage.setItem("ipip.ipdb", e.target.result); };
+        a.readAsDataURL(blob);
+    } else {
+        console.log("not support FileReader");
+    }
+}
+// dataURL转换为Blob对象
+function dataURLtoBlob(dataurl) {
+    var arr = dataurl.split(","), mime = arr[0].match(/:(.*?);/)[1],
+        bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+    while (n--) {
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new Blob([u8arr], { type: mime });
+}
+
+var LocadIPdb = self.setInterval("GetIPDB()", 5000);
+var countInterval = 0;
+var IPdbBlob;
+
+function GetIPDB() {
+    console.log(countInterval++);
+    if (IPdbBlob == null) {//判断IpdbBlob 是否存在；
+        var IPdbURL = localStorage.getItem("ipip.ipdb");
+        if (IPdbURL == null) {//本地不存在，则向网络请求
+            var url = "./data/ipip.ipdb";
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", url, true);
+            xhr.responseType = "blob";
+            xhr.onload = function () {
+                if (this.status == 200) {
+                    IPdbBlob = this.response;
+                    window.clearInterval(LocadIPdb);//关闭定时器；
+                    // localStorage.setItem("ipip.ipdb")
+                    setLocalStorage(IPdbBlob);
+                    iniGeo();
+                } else {
+                    console.log("error");
+                }
+            }
+            xhr.send();
+        } else {//本地存在，并取出IPdbBlob;闭定时器；
+            IPdbBlob = dataURLtoBlob(IPdbURL);
+            window.clearInterval(LocadIPdb);
+            iniGeo();
+        }
+    } else {//存在IpdbBlob 则关闭定时器；
+        window.clearInterval(LocadIPdb);
+    }
+}
+
+
 function addCountry(data) {
     for (var i = 0; i < data.length; i++) {
         // console.log(data[i]);
@@ -6,73 +61,41 @@ function addCountry(data) {
     return data;
 }
 
-var resultData;
-function getFileFromWeb() {
-    var url = "./data/ipip.ipdb";
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
-    xhr.responseType = "blob";
-    xhr.onload = function () {
-        if (this.status == 200) {
-            var blob = this.response;
-            if (window.FileReader) {
-                var fr = new FileReader();
-                // console.log(;
-                fr.onloadend = function (e) {
-                    var data = fr.result;
-                    resultData = new Uint8Array(data)
-                    console.log("load success");
-                    // localStorage.setItem("dbfile", blob);
-                };
-                fr.readAsArrayBuffer(blob);
-            } else {
-                console.log("not support");
-            }
-        } else {
-            console.log("error");
-        }
-    }
-    xhr.send();
-}
-getFileFromWeb();
 var city = null;
+var IPdbArrayBuffer;
+function iniGeo() {
+    var a = new FileReader();
+    a.onloadend = function (e) {
+        IPdbArrayBuffer = e.target.result;
+        console.log("IpdbArrayBuffer load success");
+        city = new City(new Uint8Array(IPdbArrayBuffer));
+    };
+    a.readAsArrayBuffer(IPdbBlob);
+}
 function getLocation(ip) {
     try {
         if (city == null) {
-            city = new City(resultData);
+            return "Null";
+            // if (IPdbArrayBuffer != null) {
+            //     city = new City(new Uint8Array(IPdbArrayBuffer));
+            // } else {
+            //     if (IPdbBlob == null) {
+            //         return "Null";
+            //     } else {
+            //         var a = new FileReader();
+            //         a.onloadend = function (e) {
+            //             IPdbArrayBuffer = e.target.result;
+            //             console.log("IpdbArrayBuffer load success");
+            //             city = new City(new Uint8Array(IPdbArrayBuffer));
+            //         };
+            //         a.readAsArrayBuffer(IPdbBlob);
+            //     }
+            // }
+        } else {
+            var ipinfo = city.findInfo(ip, "CN");
+            return ipinfo.countryName + "-" + ipinfo.regionName + "-" + ipinfo.cityName;
         }
-        var ipinfo = city.findInfo(ip, "CN");
-        return ipinfo.countryName + "-" + ipinfo.regionName + "-" + ipinfo.cityName;
     } catch (error) {
         return "Null";
     }
 }
-//online request
-// function getLocation(ip) {
-//     var area="Null";
-//     $.ajax({
-//         url: "http://47.75.245.66/api/ipredis.php?ip=" + ip,  //请求的URL
-//         timeout: 1000, //超时时间设置，单位毫秒
-//         type: 'get',  //请求方式，get或post
-//         async:false,
-//         data: {},  //请求所传参数，json格式
-//         dataType: 'json',//返回的数据格式
-//         success: function (data) { //请求成功的回调函数
-//             // alert("成功");
-//             // console.log(data);
-//             area= data[0]+"-"+data[1]+"-"+data[2];
-//         },
-//         error: function(jqXHR, textStatus, errorThrown) {
-//             /*错误信息处理*/
-//             // console.log(textStatus);
-//             // return "Null";
-//         },
-//         complete: function (XMLHttpRequest, status) { //请求完成后最终执行参数
-//             if (status == 'timeout') {//超时,status还有success,error等值的情况
-//                 // console.log("timeout");
-//                 // return "Null";
-//             }
-//         }
-//     });
-//     return area;
-// }
